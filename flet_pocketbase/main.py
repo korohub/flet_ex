6a500d09544739d8a8e657ef1e37bcb63c76b13f
david.gruny-at-674002379927 <david@advercity.fr>
2024-01-08 23:42:56 +0100

    add edit and delete

--- flet_pocketbase/main.py
+++ flet_pocketbase/main.py
@@ -9,6 +9,7 @@ def main(page: Page):
     email = TextField(label='email')
     password = TextField(label='password')
 
+    editid = TextField()
     listalluserresult = Column()
 
     def addnewuser(e):
@@ -33,10 +34,48 @@ def main(page: Page):
             print(err)
 
     def deleteuser(e):
-        pass
+        youdeleteid = e.control.data
+        admin_data = client.admins.auth_with_password("hamster@gmail.com", "nEXg0V8G3H9Th_dgF-9NFw4kXoDCh0DT")
+        alluser = client.collection("users").delete(youdeleteid)
+        listalluserresult.controls.clear()
+        get_all_user()
+
+        page.snack_bar = SnackBar(Text("Success delete", size=30), bgcolor="red")
+        page.snack_bar.open = True
+        page.update()
+
+    def savedatanow(e):
+        admin_data = client.admins.auth_with_password("hamster@gmail.com", "nEXg0V8G3H9Th_dgF-9NFw4kXoDCh0DT")
+        alluser = client.collection("users").update(editid.value, {
+            "username": username.value,
+            "email":email.value
+        })
+        listalluserresult.controls.clear()
+        get_all_user()
+        dialog.open = False
+        page.snack_bar = SnackBar(Text("Modifying  Succes", size=30), bgcolor="green500")
+        page.snack_bar.open = True
+        page.update()
+
+    dialog = AlertDialog(
+        title=Text('Edit data'),
+        content=Column([
+            username,
+            email,
+        ]),
+        actions=[
+            TextButton("Save data", on_click=savedatanow)
+        ]
+    )
 
     def edituser(e):
-        pass
+        editid.value = e.control.data.id
+        username.value = e.control.data.username
+        email.value = e.control.data.email
+        page.dialog = dialog
+        dialog.open = True
+        page.update()
+
 
     def get_all_user():
         admin_data = client.admins.auth_with_password("hamster@gmail.com", "nEXg0V8G3H9Th_dgF-9NFw4kXoDCh0DT")
@@ -45,20 +84,20 @@ def main(page: Page):
         for u in alluser:
             listalluserresult.controls.append(
                 Container(
-                    bgcolor="blue500",
+                    bgcolor="blue200",
                     padding=10,
                     content=Column([
-                        Row([Text(u.username, size=25, weight="blod"),Text(u.created)], alignment="spaceBetween"),
+                        Row([Text(u.username, size=25, weight="bold", color="black"),Text(u.created)], alignment="spaceBetween"),
                         Text(f'email: {u.email}'),
                         Text(f'verified: {u.verified}'),
                     IconButton("Delete", bgcolor="red700", icon_color="white",data=u.id, on_click=deleteuser),
-                    IconButton("Edit", bgcolor="blue", icon_color="white", data=u.id, on_click=edituser),
+                    IconButton("Edit", bgcolor="blue", icon_color="white", data=u, on_click=edituser),
                     ],alignment="spaceBetween")
                 )
             )
         page.update()
     
-
+    get_all_user()
 
     page.window_height = 740
     page.window_width = 360
