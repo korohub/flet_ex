2659bf63f173e9c8654253069eff2fd2b2522479
david.gruny-at-674002379927 <david@advercity.fr>
2024-01-10 23:13:01 +0100

    add stack with bracket

--- flet_drawer/main.py
+++ flet_drawer/main.py
@@ -4,33 +4,51 @@ from flet import *
 def main(page: Page):
     page.padding = 0
     page.spacing = 0
+    page.window_height=740
+    page.window_width=360
 
-    def hidenav(e):
-        pass
 
     def shownav(e):
-        pass
+        sidemenu.offset = transform.Offset(0,0)
+        page.update()
 
+    def hidenav(e):
+        sidemenu.offset = transform.Offset(-5,0)
+        page.update()
+
+    sidemenu = Container(
+        content=Text("hello", size=17), padding=10, bgcolor="red",
+        width=page.window_width*0.7,
+        height=page.window_height,
+        margin=margin.only(top=40),
+        offset=transform.Offset(-5,0),
+        animate_offset=animation.Animation(500)
+    )
+    
     layer = Container(
         width=page.window_width,
         height=page.window_height,
-        on_click=lambda e:hidenav
+        on_click=lambda e: hidenav(e),
         content = Column([
             Container(
                 bgcolor="blue",
                 content=Row([
                     IconButton(
-                        icon="menu",
-                        icon_color="white",
-                        on_click=shownav
+                        icon = "menu",
+                        icon_color = "white",
+                        on_click = shownav
                     ), 
-                    Text("My pets", animate_size=30, color="white")
+                    Text("My pets", size=30, color="white")
                 ])
             )
         ])
     )
+
     page.add(
-        Text("123")
+        Stack([
+                layer,
+                sidemenu
+            ])
     )
 
 
